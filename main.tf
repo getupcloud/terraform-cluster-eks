434a0587017473cc2c5ef4831502a402cefb33eb
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2021-07-24 16:05:29 -0300

    Add flux vars

--- main.tf
+++ main.tf
@@ -35,6 +35,9 @@ module "cluster" {
 
 module "flux" {
   source = "github.com/getupcloud/terraform-module-flux?ref=main"
+
+  git_repo       = var.flux_git_repo
+  manifests_path = "./clusters/${var.name}/eks/manifests"
 }
 
 module "cluster-autoscaler" {
--- variables.tf
+++ variables.tf
@@ -15,3 +15,15 @@ variable "get_kubeconfig_command" {
   description = "Command to create/update kubeconfig"
   default     = "kind export kubeconfig --name $CLUSTER_NAME"
 }
+
+variable "flux_git_repo" {
+  description = "GitRepository URL."
+  type        = string
+  default     = ""
+}
+
+variable "manifests_path" {
+  description = "Manifests dir inside GitRepository"
+  type        = string
+  default     = ""
+}
