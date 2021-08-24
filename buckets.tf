aec7aff363474a974f4ccecceb6f604c75f77f01
Lanay Marques <marqueslanay@gmail.com>
2021-08-24 18:54:58 -0300

    add tf buckets and variables

---
+++ buckets.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "buckets" {
+    for_each      = { for b in var.s3_buckets : try(b.name, "${b.name_prefix}-${var.name}-${random_string.suffix.result}") => b }
+
+    bucket = each.key
+    acl    = try(each.value.acl, "private")
+    region = each.value.region
+
+    tags = {
+    Environment = "infra"
+    }
+}
