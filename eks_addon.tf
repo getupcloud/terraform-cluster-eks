a131c34315870a58e8bbe960fb1e81240ce76d5f
Lanay Marques <marqueslanay@gmail.com>
2021-10-05 22:02:38 -0300

    Add eks add-on version

--- eks_addon.tf
+++ eks_addon.tf
@@ -2,4 +2,5 @@ resource "aws_eks_addon" "eks_addon" {
     for_each = { for b in var.eks_addon : try(b.addon_name, "${b.addon_name}") => b}
     cluster_name    = var.name
     addon_name   = try(each.value.addon_name)
+    addon_version = try(each.value.addon_version)
     }       
