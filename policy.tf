f142b4b8e4f1d670b4de4201e32eb92d2b63346d
Lanay Marques <marqueslanay@gmail.com>
2021-10-13 11:47:26 -0300

    iam policy test velero

--- policy.tf
+++ policy.tf
@@ -7,7 +7,7 @@
 
 data "template_file" "policy" {
     for_each      = { for b in var.policy_document : try(b.name, "${b.name}-${random_string.suffix.result}") => b}
-    template = "${file("terraform-cluster-eks/policy.json.tpl")}"
+    template = "${file("${path.module}/policy.json.tpl")}"
 
     vars = {
         resource = "${try(each.value.resource)}"
