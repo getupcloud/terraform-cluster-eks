3c8dc5f02e253df4e9de68f3f33067513562ff53
Lanay Marques <marqueslanay@gmail.com>
2021-10-13 11:29:41 -0300

    iam policy test velero

--- policy.tf
+++ policy.tf
@@ -1,6 +1,22 @@
+# resource "aws_iam_policy" "policy_document" {
+#     for_each      = { for b in var.policy_document : try(b.name, "${b.name}-${random_string.suffix.result}") => b}
+#     name   = each.key
+#     path   = "/"
+#     policy = "${try(each.value.policy)}"
+#     }
+
+data "template_file" "policy" {
+    for_each      = { for b in var.policy_document : try(b.name, "${b.name}-${random_string.suffix.result}") => b}
+    template = "${file("policy.json.tpl")}"
+
+    vars = {
+        resource = "${try(each.value.resource)}"
+        action = "${try(each.value.action)}"
+    }
+}
+
 resource "aws_iam_policy" "policy_document" {
     for_each      = { for b in var.policy_document : try(b.name, "${b.name}-${random_string.suffix.result}") => b}
     name   = each.key
-    path   = "/"
-    policy = "${try(each.value.policy)}"
-    }
+    policy = "${data.template_file.policy.rendered}"
+}
