904ad1048115067c46dcf99c77c9de7086f12f82
Lanay Marques <marqueslanay@gmail.com>
2021-10-14 19:29:06 -0300

    iam policy test velero

--- policy.tf
+++ policy.tf
@@ -1,22 +1,22 @@
-resource "aws_iam_policy" "policy_document" {
-    for_each      = { for b in var.policy_document : try(b.name, "${b.name}-${random_string.suffix.result}") => b}
-    name   = each.key
-    path   = "/"
-    policy = jsonencode(each.value.policy)
-    }
-
-# data "template_file" "policy" {
-#     for_each      = { for b in var.policy_document : try(b.name, "${b.name}-${b.resource}") => b}
-#     template = "${file("${path.module}/policy.json.tpl")}"
-
-#     vars = {
-#         resource = "${try(each.value.resource)}"
-#         action = jsonencode(each.value.action)
-#     }
-# }
-
 # resource "aws_iam_policy" "policy_document" {
-#     for_each = data.template_file.policy
-#     name   = "${each.key}"
-#     policy = "${data.template_file.policy[each.key].rendered}"
-# }
+#     for_each      = { for b in var.policy_document : try(b.name, "${b.name}-${random_string.suffix.result}") => b}
+#     name   = each.key
+#     path   = "/"
+#     policy = "${try(each.value.policy)}"
+#     }
+
+data "template_file" "policy" {
+    for_each      = { for b in var.policy_document : try(b.name, "${b.name}-${b.resource}") => b}
+    template = "${file("${path.module}/policy.json.tpl")}"
+
+    vars = {
+        resource = "${try(each.value.resource)}"
+        action = jsonencode(each.value.action)
+    }
+}
+
+resource "aws_iam_policy" "policy_document" {
+    for_each = data.template_file.policy
+    name   = "${each.key}"
+    policy = "${data.template_file.policy[each.key].rendered}"
+}
