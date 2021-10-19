fcd17290e09e1be7fe3daacdb7f8415cf8cd81ff
Lanay Marques <marqueslanay@gmail.com>
2021-10-19 11:52:16 -0300

    update local exec

--- local_exec.tf
+++ local_exec.tf
@@ -1,8 +1,5 @@
 resource "null_resource" "local_exec" {
   for_each = { for b in var.local_exec : try(b.command, "${b.command}") => b }
-#  triggers = {
-#    always_run = timestamp()
-#  }
   provisioner "local-exec" {
     on_failure  = fail
     when        = create
