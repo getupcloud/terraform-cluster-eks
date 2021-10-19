e290f97a426c8b1a30f0bae4cffa41ee83366c77
Lanay Marques <marqueslanay@gmail.com>
2021-10-19 11:46:36 -0300

    update local exec

--- local_exec.tf
+++ local_exec.tf
@@ -1,13 +1,17 @@
 resource "null_resource" "local_exec" {
   for_each = { for b in var.local_exec : try(b.command, "${b.command}") => b }
-  triggers = {
-    always_run = timestamp()
-  }
+#  triggers = {
+#    always_run = timestamp()
+#  }
   provisioner "local-exec" {
     on_failure  = fail
     when        = create
     interpreter = ["/bin/bash", "-c"]
     command     = try(each.value.command)
   }
+
+depends_on = [
+    aws_eks_addon.eks_addon
+  ]
 
 }
