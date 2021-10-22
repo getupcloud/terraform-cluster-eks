66b3178c2232c80cf20115c5221ade4fa68588ae
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2021-10-22 16:09:38 -0300

    typo

--- eks_addon.tf
+++ eks_addon.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_addon" "eks_addon" {
   for_each          = var.eks_addons
-  cluster_name      = module.eks.cluster_id
+  cluster_name      = module.cluster.cluster_id
   addon_name        = each.key
   addon_version     = each.value.addon_version
   resolve_conflicts = try(each.value.resolve_conflicts, "OVERWRITE")
