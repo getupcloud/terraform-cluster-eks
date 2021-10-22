a72a7fb18f52bd2da68645c7bfccbba01e162189
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2021-10-22 17:34:10 -0300

    Add outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
+output "cluster_id" {
+  description = "The name/id of the EKS cluster. Will block on cluster creation until the cluster is really ready."
+  value       = module.cluster.cluster_id
+}
+
 output "cluster_iam_role_arn" {
   description = "IAM role ARN of the EKS cluster."
-  value = module.cluster.cluster_iam_role_arn
+  value       = module.cluster.cluster_iam_role_arn
+}
+
+output "worker_security_group_id" {
+  description = "Security group ID attached to the EKS workers."
+  value       = local.worker_security_group_id
 }
