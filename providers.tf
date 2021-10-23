753ac0fbe259eeff436d0a1f819dee6597d73c67
Lanay Marques <lanay@getupcloud.com>
2021-10-23 11:08:21 -0300

    duplicate tags

--- providers.tf
+++ providers.tf
@@ -46,10 +46,6 @@ provider "aws" {
   region     = var.region
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
-
-  default_tags {
-    tags = var.tags
-  }
 }
 
 provider "kubernetes" {
