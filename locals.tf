65ca29f488b29c61a5b0f3d6fa88d3e00838d974
Lanay Marques <lanay@getupcloud.com>
2021-10-25 22:23:16 -0300

    update map role

--- locals.tf
+++ locals.tf
@@ -40,7 +40,7 @@ locals {
   )
   map_roles = concat(
     [{
-      userarn  = module.cluster.worker_iam_role_arn
+      userarn  = output.worker_iam_role_arn
       username = "system:node:{{EC2PrivateDNSName}}"
       groups   = [
            "system:bootstrappers",
