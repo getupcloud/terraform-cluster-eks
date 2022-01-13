2edd094c207adf87fb197ec86649c1155426b225
Lanay M <marqueslanay@gmail.com>
2022-01-13 17:43:55 -0300

    Update aws_modules.tf

--- aws_modules.tf
+++ aws_modules.tf
@@ -60,7 +60,7 @@ module "certmanager" {
   cluster_oidc_issuer_url = module.cluster.cluster_oidc_issuer_url
   customer_name           = var.customer
   tags                    = var.tags
-  hosted_zone_ids          = try(var.aws_modules.certmanager.hosted_zone_id, [])
+  hosted_zone_ids          = try(var.aws_modules.certmanager.hosted_zone_ids, [])
 
 }
 
