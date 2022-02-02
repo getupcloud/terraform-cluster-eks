868e5e9c5e9c2813365c6460c3c9ed131992cf36
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-02-02 12:49:14 -0300

    re-adding providers.tf

---
+++ providers.tf
@@ -0,0 +1,7 @@
+provider "kubectl" {
+  load_config_file       = false
+  host                   = local.api_endpoint
+  cluster_ca_certificate = local.certificate_authority_data
+  token                  = local.token
+  apply_retry_count      = 2
+}
