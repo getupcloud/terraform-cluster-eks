28c615c11fe388202a8deee73b4972d2b119a3d4
lanaymc <lanay@getupcloud.com>
2022-02-11 11:58:22 -0300

    fix:acl private s3

--- buckets.tf
+++ buckets.tf
@@ -2,6 +2,10 @@ resource "aws_s3_bucket" "buckets" {
   for_each = { for b in var.s3_buckets : try(b.name, "${b.name_prefix}-${var.cluster_name}-${random_string.suffix.result}") => b }
 
   bucket = each.key
-  acl    = try(each.value.acl, "private")
   tags   = try(each.value.tags)
+}
+
+resource "aws_s3_bucket_acl" "buckets_acl" {
+  bucket = aws_s3_bucket.buckets.id
+  acl    = "private"
 }
