0388f6e335e6631e9d18f39fc35fdd886b101966
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-06-13 17:12:22 -0300

    bump

--- main.tf
+++ main.tf
@@ -51,8 +51,10 @@ module "flux" {
       alertmanager_cronitor_id : module.cronitor.cronitor_id
       secret : random_string.secret.result
       suffix : random_string.suffix.result
+      modules : {
+        teleport-agent : module.teleport-agent.teleport_agent_config
+      }
     },
-    module.teleport-agent.teleport_agent_config,
     var.manifests_template_vars
   )
 }
