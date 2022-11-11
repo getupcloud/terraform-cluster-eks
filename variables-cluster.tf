7c6bb4ce163ffb8a86ffdb21ee0f7d9dce94ee93
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-11-11 11:46:24 -0300

    missing pre/post vars

---
+++ variables-cluster.tf
@@ -0,0 +1,11 @@
+variable "eks_pre_create" {
+  description = "Scripts to execute before cluster is created."
+  type        = list(string)
+  default     = []
+}
+
+variable "eks_post_create" {
+  description = "Scripts to execute after cluster is created."
+  type        = list(string)
+  default     = ["./eks-post-create.sh"]
+}
